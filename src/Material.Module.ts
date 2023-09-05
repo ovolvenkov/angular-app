0eeebdebb01b16fde7443e32fc24450ef4a17087
volvenkov <oleh.volvenkov@ewave.com>
2023-09-05 15:48:40 +0200

    added header component

--- src/Material.Module.ts
+++ src/Material.Module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from "@angular/core";
 import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
 
 @NgModule({
   exports:[
-    MatInputModule
+    MatInputModule,
+    MatButtonModule,
+    MatMenuModule
   ]
 })
 
