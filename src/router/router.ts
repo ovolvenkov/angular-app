f2ea1b271aa7bbfd6583e50142d2946e327a2f00
volvenkov <oleh.volvenkov@ewave.com>
2023-09-08 12:50:22 +0200

    added recipe edit component functionality

--- src/router/router.ts
+++ src/router/router.ts
@@ -3,12 +3,15 @@ import {RecipesComponent} from "../app/components/recipes/recipes.component";
 import {ShoppingListComponent} from "../app/components/shopping-list/shopping-list.component";
 import {RecipeStartComponent} from "../app/components/recipe-start/recipe-start.component";
 import {RecipeDetailComponent} from "../app/components/recipe-detail/recipe-detail.component";
+import {RecipeEditComponent} from "../app/components/recipe-edit/recipe-edit.component";
 
 export const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children: [
     {path: '', component: RecipeStartComponent},
-    {path: ':id', component: RecipeDetailComponent}
+    {path: 'new', component: RecipeEditComponent},
+    {path: ':id', component: RecipeDetailComponent},
+    {path: ':id/edit', component: RecipeEditComponent},
     ] },
   { path: 'shopping-list', component: ShoppingListComponent },
 ]
