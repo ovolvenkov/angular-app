91270584c619de0267278469e4e3c351fdcc5a29
volvenkov <oleh.volvenkov@ewave.com>
2023-09-11 09:28:47 +0200

    added aliases

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from "../../models/ingredient";
-import {ShoppingListService} from "../../services/shopping-list.service";
+import {ShoppingListService} from "@services/shopping-list.service";
 import {Subscription} from "rxjs";
 
 @Component({
