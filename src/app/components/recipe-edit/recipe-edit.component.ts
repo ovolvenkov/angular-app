297f0501fd099f0cdba706f38bbffe73a0d7d301
volvenkov <oleh.volvenkov@ewave.com>
2023-09-11 11:00:40 +0200

    added logic for recipe form

--- src/app/components/recipe-edit/recipe-edit.component.ts
+++ src/app/components/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
+import {FormControl, FormGroup} from "@angular/forms";
+import {RecipeService} from "@services/recipe.service";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,8 +11,12 @@ import {ActivatedRoute, Params} from "@angular/router";
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+    ) {
   }
 
   ngOnInit() {
@@ -18,8 +24,27 @@ export class RecipeEditComponent implements OnInit {
       .subscribe((params: Params) => {
         this.id = params['id'];
         this.editMode = params['id'] !== null;
+        this.initForm();
       })
+  }
 
+  private initForm() {
+    let recipeName = '';
+    let recipeImgPath = '';
+    let recipeDescription = '';
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImgPath = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      "name": new FormControl(recipeName),
+      "imagePath": new FormControl(recipeImgPath),
+      "description": new FormControl(recipeDescription),
+    })
   }
 
 }
