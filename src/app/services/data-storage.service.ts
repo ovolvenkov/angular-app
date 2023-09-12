ca8c03d189682f1da1a8cd94c88ee53b8a6f55a4
volvenkov <oleh.volvenkov@ewave.com>
2023-09-12 18:10:59 +0200

    added http request functionality

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,11 +1,13 @@
-import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
-import {RecipeService} from "@services/recipe.service";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {
-  }
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
@@ -17,5 +19,25 @@ export class DataStorageService {
       .subscribe(response => {
         console.log(response);
       });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        'https://ng-course-recipe-book-65f10.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map(recipes => {
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setRecipes(recipes);
+        })
+      )
   }
 }
